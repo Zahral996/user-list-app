4b83c5540db265d9b2018918e9ce641d22f4f143
Zahral996 <zahra.secret122@gmail.com>
2025-03-09 16:30:36 +0330

    api was added

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const usersApi = createApi({
+  reducerPath: 'usersApi', 
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://67c82dad0acf98d070854ab8.mockapi.io/api/v1/users' }), 
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => 'users',
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersApi;

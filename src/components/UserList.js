a74e869bd7e5079052066b7641433c92ddc06cc8
Zahral996 <zahra.secret122@gmail.com>
2025-03-11 09:13:04 +0330

    put loading on center

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useGetUsersQuery } from '../services/api';
 import { Favorite, FavoriteBorder, Search } from '@mui/icons-material';
 import { IconButton, InputAdornment, Pagination } from '@mui/material';
-import { List, ListItem, ListItemText, ListItemAvatar, Avatar, TextField, CircularProgress} from '@mui/material';
+import { List, ListItem, ListItemText, ListItemAvatar, Avatar, TextField, CircularProgress } from '@mui/material';
 import { addToFavorites, removeFromFavorites, setSearchQuery } from '../features/usersSlice';
 
 const UserList = () => {
@@ -26,13 +26,19 @@ const UserList = () => {
     }
   };
 
-  if (isLoading) return <CircularProgress />;
-  if (isError) return <div>Error loading users!</div>;
+  if (isLoading)
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress/>
+      </div>
+    );
+
+  if (isError) return
+   <div>Error loading users!</div>;
 
   const filteredUsers = users?.filter((user) =>
     user.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
 
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
@@ -51,16 +57,17 @@ const UserList = () => {
           startAdornment: (
             <InputAdornment position="start">
               <Search />
-            </InputAdornment> ),
+            </InputAdornment>
+          ),
         }}
       />
       <h3 className='text-3xl font-semibold pt-4 pb-1'>User's List</h3>
-      <p className='pb-4'>Select your favorite people from the list below by clicking the heart icon.</p>
+      <p className='pb-4'>Select your favorite people from the list below by clicking the heart icon. </p>
       <List className='rounded-md shadow-sm border border-gray-200'>
         {currentUsers?.map((user) => (
           <ListItem key={user.id} className={`odd:bg-gray-50 even:bg-white`}>
             <IconButton onClick={() => handleFavorite(user.id)}>
-              {favorites.includes(user.id) ? (<Favorite color="error" />) : (<FavoriteBorder />)}
+              {favorites.includes(user.id) ? <Favorite color="error" /> : <FavoriteBorder />}
             </IconButton>
             <ListItemAvatar>
               <Avatar alt={user.name} src={user.avatar}/>
